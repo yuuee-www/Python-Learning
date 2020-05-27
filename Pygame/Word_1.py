8487b08fee1db78784ab2ccaa21d90f2018e504b
CC <50982416+cyndereN@users.noreply.github.com>
2020-05-27 18:39:43 +0100

    Final seminars

---
+++ Pygame/Word_1.py
@@ -0,0 +1,16 @@
+import pygame, sys
+import pygame.freetype
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("Pygame_Word")
+GOLD = 255, 251, 0
+
+f1 = pygame.freetype.Font("C://Windows//Fonts//msyh.ttc", 36)
+f1rect = f1.render_to(screen, (200, 160), "世界和平", fgcolor=GOLD, size=50)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    pygame.display.update()
