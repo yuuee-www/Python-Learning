6550d15a213b472fe86d77447d7d1ce9ace2de3f
CC <50982416+cyndereN@users.noreply.github.com>
2020-05-27 18:18:57 +0100

    BumpBall 4 and draw

---
+++ Pygame/BumpBall_4.py
@@ -0,0 +1,66 @@
+''' Mouse Move Function'''
+
+import pygame, sys
+
+pygame.init()
+size = width, height = 600, 400
+speed = [1, 1]
+BLACK = 0, 0, 0
+screen = pygame.display.set_mode(size, pygame.RESIZABLE)  # Resizable
+
+icon = pygame.image.load("PYG03-flower.png")
+pygame.display.set_icon(icon)
+pygame.display.set_caption("BumpBall_4")
+ball = pygame.image.load("PYG02-ball.gif")
+ballrect = ball.get_rect()
+fps = 300
+fclock = pygame.time.Clock()
+still = False
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                speed[0] = speed[0] if speed[0] == 0 else (abs(speed[0]) - 1) * int(speed[0] / abs(speed[0]))
+            elif event.key == pygame.K_RIGHT:
+                speed[0] = speed[0] + 1 if speed[0] > 0 else speed[0] - 1
+            elif event.key == pygame.K_UP:
+                speed[1] = speed[1] + 1 if speed[1] > 0 else speed[1] - 1
+            elif event.key == pygame.K_DOWN:
+                speed[1] = speed[1] if speed[1] == 0 else (abs(speed[1]) - 1) * int(speed[1] / abs(speed[1]))
+            elif event.key == pygame.K_ESCAPE:
+                sys.exit()
+        elif event.type == pygame.VIDEORESIZE:
+            size = width, height = event.size[0], event.size[1]
+            screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                still = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            still = False
+            if event.button == 1:
+                ballrect = ballrect.move(event.pos[0] - (ballrect.left+ballrect.right)/2, event.pos[1] - (ballrect.top + ballrect.bottom)/2)
+        elif event.type == pygame.MOUSEMOTION:
+            if event.buttons[0] == 1:
+                ballrect = ballrect.move(event.pos[0] - (ballrect.left+ballrect.right)/2, event.pos[1] - (ballrect.top + ballrect.bottom)/2)
+    if pygame.display.get_active() and not still:
+        ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = - speed[0]
+        if ballrect.right > width and ballrect.right + speed[0] > ballrect.right:
+            speed[0] = - speed[0]
+        elif ballrect.left < 0 and ballrect.left + speed[0] < ballrect.left:
+            speed[0] = - speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = - speed[1]
+        if ballrect.bottom > height and ballrect.bottom + speed[1] > ballrect.bottom:
+            speed[1] = - speed[1]
+        elif ballrect.top < 0 and ballrect.top + speed[1] < ballrect.top:
+            speed[1] = - speed[1]
+
+    screen.fill(BLACK)
+    screen.blit(ball, ballrect)
+    pygame.display.update()
+    fclock.tick(fps)
---
+++ Pygame/draw.py
@@ -0,0 +1,29 @@
+# Unit PYG05: Pygame Shape Draw Test
+import pygame, sys
+from math import pi
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("Pygame_Draw")
+GOLD = 255, 251, 0
+RED = pygame.Color('red')
+WHITE = 255, 255, 255
+GREEN = pygame.Color('green')
+
+# r1rect = pygame.draw.rect(screen, GOLD, (100,100,200,100), 5)
+# r2rect = pygame.draw.rect(screen, RED, (210,210,200,100), 0)
+
+e1rect = pygame.draw.ellipse(screen, GREEN, (50, 50, 500, 300), 3)
+c1rect = pygame.draw.circle(screen, GOLD, (200, 180), 30, 5)
+c2rect = pygame.draw.circle(screen, GOLD, (400, 180), 30)
+r1rect = pygame.draw.rect(screen, RED, (170, 130, 60, 10), 3)
+r2rect = pygame.draw.rect(screen, RED, (370, 130, 60, 10))
+plist = [(295, 170), (285, 250), (260, 280), (340, 280), (315, 250), (305, 170)]
+l1rect = pygame.draw.lines(screen, GOLD, True, plist, 2)
+a1rect = pygame.draw.arc(screen, RED, (200, 220, 200, 100), 1.4 * pi, 1.9 * pi, 3)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    pygame.display.update()
