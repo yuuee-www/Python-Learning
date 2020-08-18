41ad443f736a298b490efbdd9fa79ab70fbb884b
CC <50982416+cyndereN@users.noreply.github.com>
2020-08-18 14:03:18 +0100

    Update newyear

--- Django/test/lecture3/hello/views.py
+++ Django/test/lecture3/hello/views.py
@@ -2,11 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+'''def index(request):
+    return HttpResponse("Hello, world!")'''
+
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
+
 
 def brian(request):
     return HttpResponse("Hello, Brian!")
 
+'''def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")'''
+
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html",{
+        "name": name.capitalize()
+    })
