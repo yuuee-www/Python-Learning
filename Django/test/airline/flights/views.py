040d3a186384020ad087a8ebe457b0c190ac51be
CC <50982416+cyndereN@users.noreply.github.com>
2020-08-26 23:49:30 +0100

    Update flights

--- Django/test/airline/flights/views.py
+++ Django/test/airline/flights/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Flights
+
 # Create your views here.
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.object.all()
+    })
